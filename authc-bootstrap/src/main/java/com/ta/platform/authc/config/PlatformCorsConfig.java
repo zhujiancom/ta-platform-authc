08dedc15cb08100d7e19829f0aae471435e601ac
zhujiancom <eric87com@gmail.com>
2020-05-25 11:56:54 +0800

    feat(authc): 添加跨域支持

---
+++ authc-bootstrap/src/main/java/com/ta/platform/authc/config/PlatformCorsConfig.java
@@ -0,0 +1,48 @@
+package com.ta.platform.authc.config;
+
+import com.ta.platform.authc.config.properties.PlatformCorsProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Creator: zhuji
+ * Date: 5/25/2020
+ * Time: 11:42 AM
+ * Description: 跨域配置
+ */
+@Slf4j
+@Configuration
+@ConditionalOnProperty(value = {"taplatform.cors.enable"}, matchIfMissing = true)
+public class PlatformCorsConfig {
+    /**
+     * CORS跨域设置
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean corsFilter(PlatformCorsProperties corsProperties) {
+        log.debug("corsProperties:{}", corsProperties);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 跨域配置
+        corsConfiguration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        corsConfiguration.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        corsConfiguration.setAllowedMethods(corsProperties.getAllowedMethods());
+        corsConfiguration.setAllowCredentials(corsProperties.isAllowCredentials());
+        corsConfiguration.setExposedHeaders(corsProperties.getExposedHeaders());
+        corsConfiguration.setMaxAge(corsConfiguration.getMaxAge());
+
+        source.registerCorsConfiguration(corsProperties.getPath(), corsConfiguration);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        bean.setEnabled(corsProperties.isEnable());
+        return bean;
+    }
+}
